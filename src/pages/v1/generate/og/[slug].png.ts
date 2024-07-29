66ba3d6c1ad375bbeb29bf20721224f3b43083c8
minortypo <gabrielpalharesdev@gmail.com>
2024-07-29 15:55:07 -0300

    fix(style): trying to fix og blogposts img

--- src/pages/v1/generate/og/[slug].png.ts
+++ src/pages/v1/generate/og/[slug].png.ts
@@ -67,7 +67,7 @@ export const GET: APIRoute = async ({ params, props }) => {
     color: white;
     line-height: 1.4;
   ">
-    <p style="display: flex">${title}</p>
+    <p style="text-align: flex">${title}</p>
   </div>
   </div>
 </div>
